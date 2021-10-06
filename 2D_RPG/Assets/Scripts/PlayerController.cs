1870df71f4d99f2b8e6bddedd7ddcb28393e742b
THORtato <37824109+THORtato@users.noreply.github.com>
2021-10-07 00:08:22 +0700

    Base Asset added, Movement implemented

---
+++ 2D_RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+    private Vector2 moveInput;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(moveInput.x, moveInput.y,0f) * Time.deltaTime * moveSpeed;
+        rb2d.velocity = moveInput * moveSpeed;
+    }
+}
