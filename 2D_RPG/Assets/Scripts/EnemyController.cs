93fcd189d0fad44d02e33db793aceaca095831dd
THORtato <37824109+THORtato@users.noreply.github.com>
2021-10-18 16:48:58 +0700

    Enemy Movement Animation Implemented

--- 2D_RPG/Assets/Scripts/EnemyController.cs
+++ 2D_RPG/Assets/Scripts/EnemyController.cs
@@ -10,25 +10,49 @@ public class EnemyController : MonoBehaviour
     public float rangeToChase;
     private Vector3 MoveDirection;
     public GameObject Player;
+    private bool isMoving = false;
+
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        EnemyMovement();
+        EnemyMovementAnimation();
+    }
+
+    private void EnemyMovement()
+    {
         if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < rangeToChase)
         {
+            isMoving = true;
             MoveDirection = PlayerController.instance.transform.position - transform.position;
         }
         else
         {
+            isMoving = false;
             MoveDirection = Vector2.zero;
         }
 
         MoveDirection.Normalize();
         rb2d.velocity = MoveDirection * speed;
+    }
+
+    public void EnemyMovementAnimation()
+    {
+        if (isMoving)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
     }
 }
